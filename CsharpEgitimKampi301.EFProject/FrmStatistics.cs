e1a5b11f534956a12fb146bbf6a7238bd1071970
Emirhan Sartik <sartikemirhan@gmail.com>
2026-08-11 15:11:47 +0300

    Keep Practice On EntityFramework

---
+++ CsharpEgitimKampi301.EFProject/FrmStatistics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpEgitimKampi301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        EgitimKampiEfTravelDbEntities db =  new EgitimKampiEfTravelDbEntities();
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+            lblLocationCount.Text = db.Location.Count().ToString();   
+            lblSumCapacity.Text = db.Location.Sum(x=> x.Capacity).ToString(); 
+            lblGuideCount.Text = db.Guide.Count().ToString();
+        }
+    }
+}
--- CsharpEgitimKampi301.EFProject/Program.cs
+++ CsharpEgitimKampi301.EFProject/Program.cs
@@ -16,7 +16,7 @@ namespace CsharpEgitimKampi301.EFProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmLocation());
+            Application.Run(new FrmStatistics());
         }
     }
 }
